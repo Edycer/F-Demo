99ca79c57c582a8cfa314679f6ddf0060698fb53
Philip Warren <philip.warren@realseriousgames.com>
2015-06-03 10:51:14 +1000

    Added start of vector lib.

---
+++ Vector/Functions.fs
@@ -0,0 +1,16 @@
+﻿module Functions
+
+open Types
+
+let subtract vector1 vector2 =
+    let x = vector1.X - vector2.X
+    let y = vector1.Y - vector2.Y
+    let z = vector1.Z - vector2.Z
+    { X = x; Y = y; Z = z }
+
+let magnitude vector =
+    sqrt ( (vector.X * vector.X) + (vector.Y * vector.Y) + (vector.Z * vector.Z) )
+
+let distanceBetween vector1 vector2 = 
+    subtract vector1 vector2 |>
+    magnitude
---
+++ Vector/Types.fs
@@ -0,0 +1,20 @@
+﻿module Types
+
+[<Measure>] type m
+[<Measure>] type cm
+[<Measure>] type mm
+
+//type Vector3 =
+//    struct
+//        val X: float<m>
+//        val Y: float<m>
+//        val Z: float<m>
+//        new (x, y, z) = {X = x; Y = y; Z = z}
+//    end
+
+type Vector3 =
+    {
+        X: float<m>
+        Y: float<m>
+        Z: float<m>
+    }
