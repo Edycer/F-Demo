f96f83e56a62c623ec73c9099ef9e795a02332b3
Adam Single <adam@adamsingle.com>
2015-06-08 21:02:17 +1000

    Added example for partially applied functions

--- Vector/Functions.fs
+++ Vector/Functions.fs
@@ -14,3 +14,42 @@ let magnitude vector =
 let distanceBetween vector1 vector2 = 
     subtract vector1 vector2 |>
     magnitude
+
+
+
+
+
+
+///
+/// Section 2
+///   
+
+//Partial Application
+
+let isWithinRange range vector1 vector2 =
+    distanceBetween vector1 vector2 < range
+    
+
+let vector1 = {X = 1.0<m>; Y = 0.0<m>; Z = 0.0<m>}
+let vector2 = {X = 2.0<m>; Y = 0.0<m>; Z = 0.0<m>}
+let vector3 = {X = 3.0<m>; Y = 0.0<m>; Z = 0.0<m>}
+let vector4 = {X = 4.0<m>; Y = 0.0<m>; Z = 0.0<m>}
+
+let spline = [vector1; vector2; vector3; vector4]
+let sphereCenter = {X = 2.5<m>; Y = 0.5<m>; Z = 0.0<m>}
+let range = 1.0<m>
+
+//A Linq style mapping function can only take one argument. We can partially apply our isWithinRange function
+//to give us a function that takes one argument in. 
+let withinRange = List.map(isWithinRange range sphereCenter) spline
+
+//Functional Composition
+
+
+//Option
+
+//Pattern Matching
+
+//Disciminated Union?
+
+//Type Provider
